4e4ee944a256c89e55f597569118177b8310d168
kharizmaaandin <kharisma.a2003@gmail.com>
2024-06-04 23:37:13 +0700

    fix chart and table

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fetchdata = async (url) => {
   const response = await fetch(url);
   return response.json();
-}
+};
 
 // SALES PER MONTH BY MACHINE
 let zalestransaction;
@@ -12,9 +12,11 @@ let librarytransaction;
 let machine;
 let zales;
 let linechart;
+let alltransactions = [];
 
 fetchdata("x1364.json").then((res) => {
   zales = res;
+  alltransactions.push(...res.transactionByProduct);
   let month = zales.transactionPerMonth.map((datum) => {
     return datum.MonthName;
   });
@@ -23,35 +25,29 @@ fetchdata("x1364.json").then((res) => {
     return datum.TransactionCount;
   });
 
-  const ctx = document.getElementById('myChart');
+  const ctx = document.getElementById("myChart");
 
   linechart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: month,
-      datasets: [{
-        label: 'LineTotal',
-        data: zalestransaction,
-        borderWidth: 1,
-        backgroundColor: 'blue'
-      }]
+      datasets: [
+        {
+          label: "LineTotal",
+          data: zalestransaction,
+          borderWidth: 1,
+          backgroundColor: "blue",
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
-
-  console.log(zales.transactionByCategory);
-  new gridjs.Grid({
-    data: zales.transactionPerMonth,
-    pagination: true,
-    sort: true,
-    search: true,
-  }).render(document.getElementById("wrapper"));
 });
 
 fetchdata("x1366.json").then((res) => {
@@ -63,6 +59,7 @@ fetchdata("x1366.json").then((res) => {
   atttransaction = att.transactionPerMonth.map((datum) => {
     return datum.TransactionCount;
   });
+  alltransactions.push(...res.transactionByProduct);
 });
 
 fetchdata("x1367.json").then((res) => {
@@ -74,6 +71,8 @@ fetchdata("x1367.json").then((res) => {
   guttentransaction = guttentransaction.transactionPerMonth.map((datum) => {
     return datum.TransactionCount;
   });
+
+  alltransactions.push(...res.transactionByProduct);
 });
 
 fetchdata("x1371.json").then((res) => {
@@ -85,6 +84,8 @@ fetchdata("x1371.json").then((res) => {
   earltransaction = earltransaction.transactionPerMonth.map((datum) => {
     return datum.TransactionCount;
   });
+
+  alltransactions.push(...res.transactionByProduct);
 });
 
 fetchdata("x1380.json").then((res) => {
@@ -96,22 +97,68 @@ fetchdata("x1380.json").then((res) => {
   librarytransaction = librarytransaction.transactionPerMonth.map((datum) => {
     return datum.TransactionCount;
   });
+
+  alltransactions.push(...res.transactionByProduct);
 });
 
-let selectElement = document.getElementsByClassName('machine-filter');
-console.log(selectElement[0].value);
+new gridjs.Grid({
+  columns: ["Product", "Category", "Quantity", "Sales"],
+  data: alltransactions,
+  pagination: true,
+  sort: true,
+  search: true,
+}).render(document.getElementById("wrapper"));
 
-const handleFilterChange = () => {
-  machine = selectElement[0].value;
-  linechart.data.datasets[0].data = machine === "BSQ Mall x1364 - Zales" ?
-    zalestransaction : machine === "BSQ Mall x1366 - ATT" ?
-      atttransaction : machine === "GuttenPlans x1367" ?
-        guttentransaction : machine === "Earle Asphalt x1371" ?
-          earltransaction : machine === "EB Public Library x1380" ?
-            librarytransaction : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let selectElementMachine = document.getElementsByClassName("machine-filter");
+
+const handleFilterChangeMachine = () => {
+  machine = selectElementMachine[0].value;
+  console.log(machine);
+
+  linechart.data.datasets[0].data =
+    machine === "BSQ Mall x1364 - Zales"
+      ? zalestransaction
+      : machine === "BSQ Mall x1366 - ATT"
+      ? atttransaction
+      : machine === "GuttenPlans x1367"
+      ? guttentransaction
+      : machine === "Earle Asphalt x1371"
+      ? earltransaction
+      : machine === "EB Public Library x1380"
+      ? librarytransaction
+      : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   linechart.update();
 };
-selectElement[0].addEventListener('change', handleFilterChange);
+
+selectElementMachine[0].addEventListener("change", handleFilterChangeMachine);
+
+const mergedTransactionByProduct = [];
+
+// let dataObject = [
+//   zales.transactionByProduct,
+//   guttentransaction.transactionByProduct,
+//   earltransaction.transactionByProduct,
+//   atttransaction.transactionByProduct,
+// ];
+
+// console.log(dataObject);
+
+// for (const dataObject of dataObjects) {
+//   for (const product of dataObject.transactionByProduct) {
+//     if (!uniqueProducts.has(product.Product)) {
+//       uniqueProducts.add(product.Product);
+//       mergedTransactionByProduct.push(product);
+//     }
+//   }
+
+//   mergedSalesPerMonth.push(...dataObject.salesPerMonth);
+// }
+
+// alltransactions = mergedTransactionByProduct;
+
+// console.log(alltransactions, "alltransactions");
+
+console.log();
 
 // REVENUE PER MACHINE
 
@@ -128,26 +175,28 @@ fetchdata("revenue.json").then((res) => {
     return datum.Revenue;
   });
 
-  const ctx = document.getElementById('myyChart').getContext('2d');
+  const ctx = document.getElementById("myyChart").getContext("2d");
 
   barchart = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: {
       labels: machine,
-      datasets: [{
-        label: 'TransTotal',
-        data: machinerevenue,
-        borderWidth: 1,
-        backgroundColor: 'purple'
-      }]
+      datasets: [
+        {
+          label: "TransTotal",
+          data: machinerevenue,
+          borderWidth: 1,
+          backgroundColor: "purple",
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
 });
 
@@ -170,92 +219,120 @@ fetchdata("mall.json").then((res) => {
     return datum.TotalProductsSold;
   });
 
-  const ctx = document.getElementById('myyyChart');
+  const ctx = document.getElementById("myyyChart");
 
   barchart = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: {
       labels: category,
       datasets: [
         {
-          label: 'Food',
-          data: mallmostselling.map((value, index) => category[index] === 'Food' ? value : 0),
+          label: "Food",
+          data: mallmostselling.map((value, index) =>
+            category[index] === "Food" ? value : 0
+          ),
           borderWidth: 1,
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)'
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
         },
         {
-          label: 'Carbonated',
-          data: mallmostselling.map((value, index) => category[index] === 'Carbonated' ? value : 0),
+          label: "Carbonated",
+          data: mallmostselling.map((value, index) =>
+            category[index] === "Carbonated" ? value : 0
+          ),
           borderWidth: 1,
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)'
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
         },
         {
-          label: 'Non Carbonated',
-          data: mallmostselling.map((value, index) => category[index] === 'Non Carbonated' ? value : 0),
+          label: "Non Carbonated",
+          data: mallmostselling.map((value, index) =>
+            category[index] === "Non Carbonated" ? value : 0
+          ),
           borderWidth: 1,
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)'
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
         },
         {
-          label: 'Water',
-          data: mallmostselling.map((value, index) => category[index] === 'Water' ? value : 0),
+          label: "Water",
+          data: mallmostselling.map((value, index) =>
+            category[index] === "Water" ? value : 0
+          ),
           borderWidth: 1,
-          backgroundColor: 'rgba(153, 102, 255, 0.2)',
-          borderColor: 'rgba(153, 102, 255, 1)'
-        }
-      ]
+          backgroundColor: "rgba(153, 102, 255, 0.2)",
+          borderColor: "rgba(153, 102, 255, 1)",
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
-  
+});
 
-  'use strict';
+let selectElementLocation = document.querySelector("#location-filter");
+console.log(selectElementLocation.value);
 
-  /*navbar*/
+const handleFilterChangeLocation = () => {
+  location = selectElementLocation[0].value;
+  console.log(location);
+  barchart.data.datasets[0].data =
+    location === "Brunswick Sq Mall"
+      ? mallmostselling
+      : location === "Earle Asphalt"
+      ? earlmostselling
+      : location === "EB Public Library"
+      ? librarymostselling
+      : location === "GuttenPlans"
+      ? guttenmostselling
+      : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  barchart.update();
+};
 
-  const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-  const navbar = document.querySelector("[data-navbar]");
-  const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-  const overlay = document.querySelector("[data-overlay]");
+selectElementLocation[0].addEventListener("change", handleFilterChangeLocation);
 
-  const elemArr = [navCloseBtn, overlay, navOpenBtn];
+("use strict");
 
-  for (let i = 0; i < elemArr.length; i++) {
-    elemArr[i].addEventListener("click", function () {
-      navbar.classList.toggle("active");
-      overlay.classList.toggle("active");
-    });
-  }
+/*navbar*/
 
-  /*mengalihkan ketika meng-klik navbar apapun*/
-  const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
 
-  for (let i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].addEventListener("click", function () {
-      navbar.classList.toggle("active");
-      overlay.classList.toggle("active");
-    });
-  }
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
 
-  /*header & go-top-btn activeketika scroll*/
-  const header = document.querySelector("[data-header]");
-  const goTopBtn = document.querySelector("[data-go-top]");
-
-  window.addEventListener("scroll", function () {
-    if (window.scrollY >= 400) {
-      header.classList.add("active");
-      goTopBtn.classList.add("active");
-    } else {
-      header.classList.remove("active");
-      goTopBtn.classList.remove("active");
-    }
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
   });
+}
+
+/*mengalihkan ketika meng-klik navbar apapun*/
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/*header & go-top-btn activeketika scroll*/
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 400) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
 });
